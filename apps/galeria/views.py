37dbaec9b2441eb5ebdf29c715d10cccbc1825f4
Keven Andrade <nivekandrade@live.com>
2023-05-02 17:14:36 -0300

    adicionando fotos em index.html

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -5,7 +5,7 @@ from apps.galeria.models import Fotografia
 def index(request):
     fotografias = Fotografia.objects.all()
 
-    return render(request, 'galeria/index.html', {'fotografia': fotografias})
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
