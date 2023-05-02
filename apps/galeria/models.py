96da2c1f64928ec6755c37e81e74d3653c769b86
Keven Andrade <nivekandrade@live.com>
2023-05-02 17:33:08 -0300

    adic campo foto em models galeria

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -6,7 +6,7 @@ class Fotografia(models.Model):
     nome = models.CharField(max_length=130, blank=False, null=False)
     legenda = models.CharField(max_length=130, blank=False, null=False)
     descricao = models.TextField(null=False, blank=False)
-
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d', blank=True)
 
     def __str__(self):
         return self.nome
