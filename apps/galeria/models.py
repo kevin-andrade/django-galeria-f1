38eab56f3ae86ef3548f966972a31b2e38e25794
Keven Andrade <nivekandrade@live.com>
2023-05-03 15:51:26 -0300

    adic field.categoria em models

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -3,8 +3,18 @@ from django.db import models
 
 class Fotografia(models.Model):
 
+    OPCOES_CATEGORIA = [
+        ("ALPINE", 'Alpine'),
+        ("ASTON MARTIN", 'Aston Martin'),
+        ("FERRARI", 'Ferrari'),
+        ("RED BULL", 'Red Bull'),
+        ("MERCEDES", 'Mercedes'),
+        ("MCLAREN", 'Mclaren'),
+    ]
+
     nome = models.CharField(max_length=130, blank=False, null=False)
     legenda = models.CharField(max_length=130, blank=False, null=False)
+    categoria = models.CharField(max_length=100, null=False, choices=OPCOES_CATEGORIA, default="" )
     descricao = models.TextField(null=False, blank=False)
     foto = models.ImageField(upload_to='fotos/%Y/%m/%d', blank=True)
 
